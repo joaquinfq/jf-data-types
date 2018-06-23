38a80059f7d3fe13dae98d4d4f212b109cdb33b3
Joaquín Fernández <jf@joaquinfernandez.net>
2018-06-23 16:15:37 +0200

    "Agregado el método `call` para llamar a métodos de la clase `Array`"

--- src/Array.mjs
+++ src/Array.mjs
@@ -32,16 +32,14 @@ export default class jfDataTypeArray extends jfDataTypeBase
             if (value.length)
             {
                 const _Class     = this.constructor;
-                const _defaults  = _Class.defaults;
-                const _config    = _defaults.config;
+                const _config    = _Class.defaults.config;
                 const _hasConfig = _config && typeof _config === 'object';
-                const _type      = _defaults.type;
                 value            = value
                     .map(
                         value => {
-                            if (!(value instanceof jfDataTypeBase))
+                            if (!_Class.isItem(value))
                             {
-                                value = _Class.create(_type, value);
+                                value = _Class.buildItem(value);
                                 if (value && _hasConfig)
                                 {
                                     value.setProperties(_config);
@@ -87,6 +85,80 @@ export default class jfDataTypeArray extends jfDataTypeBase
                 };
             }
         };
+    }
+
+    /**
+     * Construye un elemento del array.
+     *
+     * @param  {Object} config Configuración del elemento a construir.
+     *
+     * @return {jf.dataType.Base|null} Elemento del tipo válido o `null`.
+     *
+     * @static
+     */
+    static buildItem(config)
+    {
+        let _item = null;
+        if (config && !this.isItem(config))
+        {
+            const _defaults = this.defaults;
+            _item           = this.create(_defaults.type, config);
+            if (_item && _defaults.config)
+            {
+                _item.setProperties(_defaults.config)
+            }
+        }
+
+        return _item;
+    }
+
+    /**
+     * Ejecuta el método de un array como si fuera un método de la instancia.
+     * Esto permitir acceder a algunas funciones útiles presentes en los array
+     * pero que no existen en la clase debido a no poder extender de `Array`.
+     *
+     * @method apply
+     *
+     * @param {String} fn   Nombre del método.
+     * @param {*}      args Argumentos de la función.
+     *
+     * @return {*} Valor retornado por la función ejecutada.
+     */
+    call(fn, ...args)
+    {
+        const _items = this.raw();
+
+        return Array.isArray(_items) && typeof _items[fn] === 'function'
+            ? _items[fn](...args)
+            : undefined;
+    }
+
+    /**
+     * Verifica si el elemento corresponde es del tipo de dato manejado por la clase.
+     *
+     * @param {*} item Elemento a verificar.
+     *
+     * @return {Boolean} `true` si el elemento es un tipo de dato válido para la clase.
+     *
+     * @static
+     */
+    static isItem(item)
+    {
+        let _is = false;
+        if (item)
+        {
+            const _type = this.defaults.type;
+            if (_type)
+            {
+                const _Class = this.factory.get(_type);
+                if (_Class)
+                {
+                    _is = item instanceof _Class;
+                }
+            }
+        }
+
+        return _is;
     }
 
     /**
--- tests/unit/Array.mjs
+++ tests/unit/Array.mjs
@@ -4,16 +4,20 @@ import jfDataTypeArray  from '@/Array';
 import jfDataTypeBase   from '@/Base';
 import jfDataTypeString from '@/String';
 
+function createInstance(values, Class = jfDataTypeArray)
+{
+    const _array = new Class();
+    if (values)
+    {
+        _array.value = values;
+    }
+
+    return _array;
+}
+
 function createTypes(Class, values)
 {
-    return values.map(
-        value => {
-            const _type = new Class();
-            _type.value = value;
-
-            return _type;
-        }
-    )
+    return values.map(value => createInstance(value, Class))
 }
 
 base(jfDataTypeArray, jfDataTypeBase);
@@ -36,80 +40,187 @@ describe(
     }
 );
 describe(
-    'jfDataTypeArray - parser - jfDataTypeString',
+    'jfDataTypeArray - buildItem',
     () => {
-        const _value = createTypes(jfDataTypeString, [ 1, 2, 3 ]);
-        const _sut   = new jfDataTypeArray();
-        _sut.value   = _value;
-        expect(_sut.raw()).toEqual(_value);
+        it(
+            'Valor falsy devuelve null',
+            () => [ undefined, null, false ].forEach(
+                value => expect(jfDataTypeArray.buildItem(value)).toBeNull()
+            )
+        );
+        it(
+            'Mismo tipo no lo construye y devuelve null',
+            () => expect(jfDataTypeArray.buildItem(jfDataTypeArray.create(jfDataTypeArray.defaults.type))).toBeNull()
+        );
+        it(
+            'Diferente tipo lo construye',
+            () => {
+                const _item = jfDataTypeArray.buildItem('lorem ipsum');
+                expect(_item).toBeInstanceOf(jfDataTypeString);
+                expect(_item.value).toBe('lorem ipsum');
+            }
+        );
     }
 );
 describe(
-    'jfDataTypeArray - parser - Escalares',
+    'jfDataTypeArray - call',
     () => {
-        const _value = [ 1, 2, 3 ];
-        const _sut   = new jfDataTypeArray();
-        _sut.value   = _value;
-        expect(_sut.raw()).toEqual(createTypes(jfDataTypeString, [ 1, 2, 3 ]));
+        const _values = [1, 2, 3, 4];
+        const _sut    = createInstance(_values);
+        // Iteración con una función existente
+        it(
+            'Función inexistente',
+            () => {
+                let _count = 0;
+                let _result = _sut.call(
+                    'map',
+                    (item, index) => {
+                        const _value = parseInt(item.value, 10);
+                        expect(_value).toBe(_values[index]);
+                        ++_count;
+
+                        return _value * _value;
+                    }
+                );
+                expect(_count).toBe(_values.length);
+                expect(_result).toEqual(_values.map(value => value * value));
+            }
+        );
+        it(
+            'Función inexistente',
+            () => {
+                let _count = 0;
+                let _result = _sut.call('abcdefg', () => ++_count);
+                expect(_count).toBe(0);
+                expect(_result).toBeUndefined();
+            }
+        );
     }
 );
 describe(
-    'jfDataTypeArray - parser - Tipo sin registrar',
+    'jfDataTypeArray - isItem',
     () => {
-        const _type  = jfDataTypeArray.defaults.type;
-        jfDataTypeArray.defaults.type = 'UnknownClass';
-        const _value = [ 1, 2, 3 ];
-        const _sut   = new jfDataTypeArray();
-        _sut.value   = _value;
-        expect(_sut.valueOf()).toEqual([]);
-        jfDataTypeArray.defaults.type = _type;
+        it(
+            'Valor falsy devuelve false',
+            () => [ undefined, null, false ].forEach(
+                value => expect(jfDataTypeArray.isItem(value)).toBe(false)
+            )
+        );
+        it(
+            'Si type no está definido devuelve false',
+            () => {
+                const _Class = class extends jfDataTypeArray {
+                    static defaults = {};
+                };
+                expect(_Class.isItem({})).toBe(false);
+            }
+        );
     }
 );
 describe(
-    'jfDataTypeArray - valueOf() - Escalares',
+    'jfDataTypeArray - parser',
     () => {
         const _value = [ 1, 2, 3 ];
-        const _sut   = new jfDataTypeArray();
-        _sut.value   = [];
-        expect(_sut.valueOf()).toEqual([]);
-        _sut.$$value = [..._value];
-        expect(_sut.valueOf()).toEqual(_value);
+        it(
+            'Tipo por defecto',
+            () => {
+                const _value = createTypes(jfDataTypeString, [ 1, 2, 3 ]);
+                expect(createInstance(_value).raw()).toEqual(_value);
+            }
+        );
+        it(
+            'jfDataTypeArray - parser - Escalares',
+            () => expect(createInstance(_value).raw()).toEqual(createTypes(jfDataTypeString, [ 1, 2, 3 ]))
+        );
+        it(
+            'jfDataTypeArray - parser - Tipo sin definir',
+            () => {
+                const _Class = class extends jfDataTypeArray {
+                    static defaults = {};
+                };
+                const _sut   = createInstance(_value, _Class);
+                expect(_sut.valueOf()).toEqual([]);
+            }
+        );
+        it(
+            'jfDataTypeArray - parser - Tipo sin registrar',
+            () => {
+                const _Class = class extends jfDataTypeArray {
+                    static defaults = {
+                        type : 'UnregisteredType'
+                    };
+                };
+                const _sut   = createInstance(_value, _Class);
+                expect(_sut.valueOf()).toEqual([]);
+            }
+        );
     }
 );
 describe(
-    'jfDataTypeArray - valueOf() - defaults.type',
+    'jfDataTypeArray - valueOf',
     () => {
-        // En defaults se asigna el tipo por defecto a String.
         const _value = [ 1, 2, 3 ];
-        const _sut   = new jfDataTypeArray();
-        _sut.value   = _value;
-        expect(_sut.valueOf()).toEqual(_value.map(String));
+        it(
+            'Escalares',
+            () =>
+            {
+                const _sut = createInstance([]);
+                expect(_sut.valueOf()).toEqual([]);
+                _sut.$$value = [..._value];
+                expect(_sut.valueOf()).toEqual(_value);
+            }
+        );
+        it(
+            'defaults.type',
+            () => {
+                // En defaults se asigna el tipo por defecto a String.
+                const _sut = createInstance(_value);
+                expect(_sut.valueOf()).toEqual(_value.map(String));
+            }
+        );
     }
 );
 describe(
     'jfDataTypeArray - for...of',
     () => {
-        const _items = [ 'a', 'b', 'c', 'd', 'e', 'f' ];
-        const _sut   = new jfDataTypeArray();
-        _sut.value   = _items;
-        const _value = [];
-        for (const _v of _sut)
-        {
-            _value.push(_v.valueOf());
-        }
-        expect(_value).toEqual(_items);
-    }
-);
-describe(
-    'jfDataTypeArray - for...of con valor inválido',
-    () => {
-        const _sut   = new jfDataTypeArray();
-        _sut.value   = {};
-        const _value = [];
-        for (const _v of _sut)
-        {
-            _value.push(_v.valueOf());
-        }
-        expect(_value).toEqual([]);
+        it(
+            'Sin inicializar',
+            () =>
+            {
+                const _sut = createInstance();
+                let _count = 0;
+                for (const _ of _sut)
+                {
+                    ++_count;
+                }
+                expect(_count).toBe(0);
+            }
+        );
+        it(
+            'Listado de valores',
+            () =>
+            {
+                const _expected = [ 'a', 'b', 'c', 'd', 'e', 'f' ];
+                const _sut      = createInstance(_expected);
+                const _values   = [];
+                for (const _v of _sut)
+                {
+                    _values.push(_v.valueOf());
+                }
+                expect(_values).toEqual(_expected);
+            }
+        );
+        it(
+            'Con valor inválido',
+            () => {
+                const _sut   = createInstance({});
+                const _value = [];
+                for (const _v of _sut)
+                {
+                    _value.push(_v.valueOf());
+                }
+                expect(_value).toEqual([]);
+            }
+        );
     }
 );
